239e79e2755d3243c8c9b1f8d385f6ede9e40779
Marcel Perin <mapirpg@gmail.com>
2025-04-02 09:40:15 -0300

    aula 01-04-2025

---
+++ ppi-I/2025-04-01/1.js
@@ -0,0 +1,32 @@
+// // Hello World no console
+// console.log('Hello World!')
+// // Hllo World com uma janela de alerta
+// alert('Hello World!')
+
+// nome = "Fulano"
+// // Imprimindo tipo de variável
+// console.log(typeof nome)
+// nome = 10
+// console.log(typeof nome)
+
+// alert(nome)
+
+// const nome = prompt("Qual é o seu nome?")
+// const sobrenome = prompt("Qual é o seu sobrenome?")
+
+// console.log(nome + " " + sobrenome)
+// alert(nome + " " + sobrenome)
+
+let a = prompt("Digite um número")
+let b = prompt("Digite outro número")
+
+a = Number(a)
+b = Number(b)
+
+// alert(num1 + num2)
+
+if ((a + b) > 18 ) {
+  alert(`${a} + ${b} é Maior que 18`)
+} else {
+  alert(`${a} + ${b} é Menor que 18`)
+}
